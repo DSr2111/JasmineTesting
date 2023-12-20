6d7c14271635b40952397ea7608a6f33f420e8a1
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2023-12-20 09:53:33 -0500

    First Jasmine Tests with Tax Brackets

---
+++ script.test.js
@@ -0,0 +1,7 @@
+it("should calculate the high tax bracket", function () {
+  expect(calculateTaxes(50000)).toEqual(12500);
+  expect(calculateTaxes(100000)).toEqual(25000);
+});
+it("should calculate the low tax bracket", function () {
+  expect(calculateTaxes(10000)).toEqual(1500);
+});
